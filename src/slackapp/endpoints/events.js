7b475ce1d458714dac3ccefdda84755d6f12a8f7
Michael Maurer <mjmaurer777@gmail.com>
2020-04-30 14:42:26 -0400

    fix: turn off home

--- src/slackapp/endpoints/events.js
+++ src/slackapp/endpoints/events.js
@@ -1,16 +1,16 @@
 const { createEventAdapter } = require("@slack/events-api");
-const { openHomeWithSections } = require("~slack/home");
+// const { openHomeWithSections } = require("~slack/home");
 
 const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
 
 slackEvents.on("error", console.error);
-slackEvents.on("app_home_opened", async event => {
-  try {
-    openHomeWithSections(event.user, ["base"]);
-  } catch (error) {
-    console.error(`Error opening home page: ${error}`);
-    openHomeWithSections(event.user, ["base"]);
-  }
-});
+// slackEvents.on("app_home_opened", async event => {
+//   try {
+//     openHomeWithSections(event.user, ["base"]);
+//   } catch (error) {
+//     console.error(`Error opening home page: ${error}`);
+//     openHomeWithSections(event.user, ["base"]);
+//   }
+// });
 
 module.exports = slackEvents.requestListener();
